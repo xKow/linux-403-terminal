4441d2bf8bf7f30a643829cb23cfd44ca96a8497
xKow <simon_k_s@outlook.dk>
2024-03-03 22:46:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ document.addEventListener("DOMContentLoaded", function() {
     var terminalContent = document.getElementById("terminal-content");
     var maxLines = 17; // Antal linjer, terminalen skal indeholde
     var firstMessageAdded = false; // Flag for at spore, om den første besked er tilføjet
+// Få adgang til det ønskede element
+var terminalContent = document.getElementById("terminal-content");
+
+// Deaktiver spellcheck for elementet
+terminalContent.spellcheck = false;
 
     // Funktion til at tilføje ny tekst til terminalen med fade-in-animation
     function addTextToTerminal(text, color) {
@@ -27,25 +32,39 @@ document.addEventListener("DOMContentLoaded", function() {
         if (inputText.toLowerCase() === "help") {
             // Tilføj besked med en forsinkelse på 0.2 sekunder
             setTimeout(function() {
-                addTextToTerminal("\n\nwelcome to the xkow.xyz command center\n\ncommands:\n  help     shows this menu\n  clear    clears the terminal", "green");
+                addTextToTerminal("\n\nwelcome to the xkow.xyz command center\n\ncommands:\n  help          shows this menu\n  clear         clears the terminal\n  myip          shows your ip\n  whyamihere    answers all your questions\n  github        directs you to the person who made this", "green");
             }, 200); // 200 millisekunders forsinkelse (0.2 sekunder)
             addTextToTerminal(inputText, "white"); // Tilføj besked om hjælp med det samme, men i hvid farve
         } else if (inputText.toLowerCase() === "clear") {
             // Håndter "clear" kommando
             terminalContent.innerHTML = ""; // Tøm indholdet af terminalen
             firstMessageAdded = false; // Nulstil flagget for den første besked
+        } else if (inputText.toLowerCase() === "whyamihere") {
+            // Håndter "whyamihere" kommando
+            setTimeout(function() {
+                addTextToTerminal("\n\nyou're on this page because you tried to access something that you didn't have the right permission to", "green");
+            }, 200);
+            addTextToTerminal(inputText, "white"); // Tilføj besked om hjælp med det samme, men i hvid farve
+        } else if (inputText.toLowerCase() === "github") {
+            // Håndter "whyamihere" kommando
+            setTimeout(function() {
+                addTextToTerminal("\n\ngithub/xkow", "green");
+            }, 200);
+            addTextToTerminal(inputText, "white"); // Tilføj besked om hjælp med det samme, men i hvid farve
+
         } else {
-            // Hvis det ikke er "help" eller "clear" kommando, tilføj teksten med det samme
+            // Hvis det ikke er en kendt kommando, tilføj teksten med det samme
             addTextToTerminal(inputText, "white");
         }
     }
+    
   
     // Fokuser input-linjen, når DOM'en er blevet fuldt indlæst
     inputLine.focus();
   
     // Tilføj initialbesked til terminalen ved indlæsningstidspunktet
-    addTextToTerminal("Error 403: Type 'help' for more information.", "red");
-    
+    addTextToTerminal("Error 403: You don't have access to this page!", "red");
+    addTextToTerminal("Type 'help' for more information.");
 
     // Lyt efter tastaturhændelser på input-linjen
     inputLine.addEventListener("keydown", function(event) {
