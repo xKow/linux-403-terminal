e4795015e6842947a85d6482dbf6cd342b4d6983
xKow <simon_k_s@outlook.dk>
2024-03-03 21:08:58 +0100

    fix

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ document.addEventListener("DOMContentLoaded", function() {
       // Hvis Enter-tasten trykkes
       if (event.key === "Enter") {
         // Få teksten fra input-linjen
-        var inputText = inputLine.innerHTML.trim();
-        // Tilføj det, der er skrevet, til terminal-indholdet
-        terminalContent.innerHTML += "<div>root@xkow.xyz: " + inputText + "</div>";
+        var inputText = inputLine.textContent.trim();
+        // Tilføj det, der er skrevet, til terminal-indholdet med "root@xkow.xyz"
+        terminalContent.innerHTML += "<div style='color: green;'>root@xkow.xyz: " + inputText + "</div>";
         // Ryd input-linjen
-        inputLine.innerHTML = "";
+        inputLine.textContent = "";
         // Forhindre standard opførsel af Enter-tasten
         event.preventDefault();
       }
