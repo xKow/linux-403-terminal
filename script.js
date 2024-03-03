ed8573de015211427904375f5ca15ec80059a406
xKow <simon_k_s@outlook.dk>
2024-03-03 22:31:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ document.addEventListener("DOMContentLoaded", function() {
     var inputLine = document.getElementById("input-line");
     var terminalContent = document.getElementById("terminal-content");
     var maxLines = 17; // Antal linjer, terminalen skal indeholde
+    var firstMessageAdded = false; // Flag for at spore, om den første besked er tilføjet
 
     // Funktion til at tilføje ny tekst til terminalen med fade-in-animation
-    function addTextToTerminal(text) {
-        var newContent = "<div style='animation: fadeIn 0.5s ease forwards;'>root@xkow.xyz:~# " + text + "</div>";
+    function addTextToTerminal(text, color) {
+        var newContent;
+        if (!firstMessageAdded) { // Hvis det er den første besked
+            newContent = "<div style='animation: fadeIn 0.5s ease forwards;'>root@xkow.xyz:~# <span style='color: " + color + ";'>" + text + "</span></div>";
+            firstMessageAdded = true; // Angiv flag til true, så vi ved, at den første besked er blevet tilføjet
+        } else {
+            newContent = "<div style='animation: fadeIn 0.5s ease forwards;'>root@xkow.xyz:~# " + text + "</div>"; // Hvis det ikke er den første besked, tilføj uden ekstra stilarter
+        }
         terminalContent.insertAdjacentHTML('beforeend', newContent);
 
         // Fjern den ældste tekst, hvis terminalen er blevet for lang
@@ -13,12 +20,31 @@ document.addEventListener("DOMContentLoaded", function() {
             terminalContent.removeChild(terminalContent.children[0]);
         }
     }
+
+    // Funktion til at håndtere kommandoer
+    function handleCommand(inputText) {
+        // Håndter "help" kommando
+        if (inputText.toLowerCase() === "help") {
+            // Tilføj besked med en forsinkelse på 0.2 sekunder
+            setTimeout(function() {
+                addTextToTerminal("lorem ipsum og lidt mere", "green");
+            }, 200); // 200 millisekunders forsinkelse (0.2 sekunder)
+            addTextToTerminal(inputText, "white"); // Tilføj besked om hjælp med det samme, men i hvid farve
+        } else if (inputText.toLowerCase() === "clear") {
+            // Håndter "clear" kommando
+            terminalContent.innerHTML = ""; // Tøm indholdet af terminalen
+            firstMessageAdded = false; // Nulstil flagget for den første besked
+        } else {
+            // Hvis det ikke er "help" eller "clear" kommando, tilføj teksten med det samme
+            addTextToTerminal(inputText, "white");
+        }
+    }
   
     // Fokuser input-linjen, når DOM'en er blevet fuldt indlæst
     inputLine.focus();
   
     // Tilføj initialbesked til terminalen ved indlæsningstidspunktet
-    addTextToTerminal("Error 403: Skriv 'help' for mere information.");
+    addTextToTerminal("Error 403: Skriv 'help' for mere information.", "red");
     
 
     // Lyt efter tastaturhændelser på input-linjen
@@ -32,8 +58,8 @@ document.addEventListener("DOMContentLoaded", function() {
         // Forhindre standard opførsel af Enter-tasten
         event.preventDefault();
         
-        // Tilføj den indtastede tekst til terminalen
-        addTextToTerminal(inputText);
+        // Behandle kommando med forsinkelse på 0.2 sekunder
+        handleCommand(inputText);
       }
     });
   
